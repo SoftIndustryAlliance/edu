152c86446448c62a6c38481668df342e9c437f80
Anton Liakh <ant.pochta@gmail.com>
2020-05-21 22:15:37 +0300

    Bugfix.

--- tests/OOP/BehavioralTest.php
+++ tests/OOP/BehavioralTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 use OOP\Behavioral\ChainOfResponsibility;
 use Faker;
 
-final class StructuralTest extends TestCase
+final class BehavioralTest extends TestCase
 {
     protected $faker;
 
